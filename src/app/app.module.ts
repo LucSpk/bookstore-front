99fb9f9629610ccd7ab3413bf8651e0ebc07860a
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-18 23:53:24 -0300

    metodo CREATE implementado para categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 
 
 
@@ -29,7 +35,8 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     FooterComponent,
     NavComponent,
     HomeComponent,
-    CategoriaReadComponent
+    CategoriaReadComponent,
+    CategoriaCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -42,6 +49,10 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     MatCardModule,
     MatTableModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
