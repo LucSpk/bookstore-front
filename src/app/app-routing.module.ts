5b1a289e9a7d2b05f6ef22cdadf05fa267b2e5a8
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-19 23:21:31 -0300

    FindAll por categoria para Livros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CategoriaDeleteComponent } from './components/views/categoria/categoria
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { HomeComponent } from './components/views/home/home.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'categorias/create', component: CategoriaCreateComponent }, 
   { path: 'categorias/delete/:id', component: CategoriaDeleteComponent }, 
   { path: 'categorias/update/:id', component: CategoriaUpdateComponent }, 
+  { path: 'categorias/:id_cat/livros', component: LivroReadAllComponent }, 
 ];
 
 @NgModule({
---
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Livro } from './livro.model';
+import { LivroService } from './livro.service';
+
+@Component({
+  selector: 'app-livro-read-all',
+  templateUrl: './livro-read-all.component.html',
+  styleUrls: ['./livro-read-all.component.css']
+})
+export class LivroReadAllComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'titulo', 'livros', 'acoes'];
+
+  livros: Livro[] = []
+  id_cat: String = "";
+
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute, 
+    ) { }
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
+    this.fiindAll();
+  }
+
+  fiindAll(): void {
+    this.service.findAllByCategoria(this.id_cat).subscribe((response) => {
+      this.livros = response
+    })
+  }
+
+}
